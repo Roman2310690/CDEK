4e1ae627a222d4d4da7424a7898790b428360254
Роман Зиновец <r.zinovets1962@gmail.com>
2021-08-25 17:00:35 +0300

    MADE THE DESIGN

---
+++ CDEK/CDEK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CDEK
+//
+//  Created by Roma on 8/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
